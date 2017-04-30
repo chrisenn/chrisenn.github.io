0f3d88503b8070dcb5b1fec8e82beba0deba60ee
Chris <christopher.hoanglinh.nguyen@gmail.com>
2017-04-30 11:56:31 -0400

    Smooth Scroll

--- javascript.js
+++ javascript.js
@@ -34,3 +34,19 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
